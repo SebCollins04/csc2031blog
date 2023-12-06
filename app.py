04c176dce227943a264f501434a1ea775fac7f23
SebCollins04 <sebcollins04@gmail.com>
2023-12-06 16:28:58 +0000

    done 2 error checking

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import secrets
 
 app = Flask(__name__)
@@ -16,3 +16,11 @@ app.register_blueprint(users_blueprint)
 
 if __name__ == '__main__':
     app.run()
+
+@app.errorhandler(404)
+def function_name(not_found):
+    return render_template('errors/404.html'), 404
+
+@app.errorhandler(500)
+def function_name(internal_server_error):
+    return render_template('errors/500.html'), 500
